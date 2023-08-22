5550c7c3d1b36aea582daa6919948a584b7f8561
exreiger <jrhall1987@gmail.com>
2023-08-22 15:02:37 -0700

    first commit

---
+++ src/main/kotlin/com/F_B/fish_brick/FishBrickApplication.kt
@@ -0,0 +1,10 @@
+package com.F_B.fish_brick
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class FishBrickApplication
+
+fun main(args: Array<String>) runApplication<FishBrickApplication>(*args)
+}
---
+++ src/test/kotlin/com/F_B/fish_brick/FishBrickApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.F_B.fish_brick
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class FishBrickApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
